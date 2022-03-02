578f6a53f83aa5b700ee66c32f275298077271c8
daoleno <daodaoonline@gmail.com>
2022-03-02 15:33:15 +0800

    Overrides the default viewport.

--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,11 @@
+import Head from "next/head";
+
 export default function Layout({ children }) {
   return (
     <div className="font-sans flex flex-col items-center mx-auto px-4 sm:px-6 lg:px-8 mt-1">
+      <Head>
+        <meta name="viewport" content="viewport-fit=cover" />
+      </Head>
       <main>{children}</main>
     </div>
   );
