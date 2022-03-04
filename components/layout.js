6733151290edab9638ba723d6464360e33aad84e
daoleno <daodaoonline@gmail.com>
2022-03-04 09:56:05 +0800

    Add Open Graph Tag and Twitter Card.

--- components/layout.js
+++ components/layout.js
@@ -5,6 +5,28 @@ export default function Layout({ children }) {
     <div className="font-sans flex flex-col items-center mx-auto px-4 sm:px-6 lg:px-8 mt-1">
       <Head>
         <meta name="viewport" content="viewport-fit=cover" />
+        {/* Open Graph */}
+        <meta property="og:title" content="Web3 by Example" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://web3byexample.com" />
+        <meta property="og:image" content="https://web3byexample.com/og.png" />
+        <meta
+          property="og:description"
+          content="A collection of web3 examples"
+        />
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@dao_leno" />
+        <meta name="twitter:creator" content="@dao_leno" />
+        <meta name="twitter:title" content="Web3 by Example" />
+        <meta
+          name="twitter:description"
+          content="A collection of web3 examples"
+        />
+        <meta name="twitter:image" content="https://web3byexample.com/og.png" />
+
+        <title>Web3 by Example</title>
+        <meta name="description" content="A collection of web3 examples" />
       </Head>
       <main>{children}</main>
     </div>
